0c51b70d8867d3140cbe52aeb0746c442e7734f1
cameron mcinnes <crm2193@columbia.edu>
2018-03-09 18:25:15 -0500

    Disable redux devtools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,20 +4,25 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  if (process.env.NODE_ENV === 'production') {
-    return createStore(
-      rootReducer,
-      preloadedState,
-      applyMiddleware(thunk, logger)
-    )
-  } else {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    return createStore(
-      rootReducer,
-      preloadedState,
-      composeEnhancers(applyMiddleware(thunk, logger))
-    )
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   return createStore(
+  //     rootReducer,
+  //     preloadedState,
+  //     applyMiddleware(thunk, logger)
+  //   )
+  // } else {
+  //   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  //   return createStore(
+  //     rootReducer,
+  //     preloadedState,
+  //     composeEnhancers(applyMiddleware(thunk, logger))
+  //   )
+  // }
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(thunk, logger)
+  )
 };
 
 export default configureStore;
