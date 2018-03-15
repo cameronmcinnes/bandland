bad8315255d8fd307874994dbf998d2b9c0fa268
cameron mcinnes <crm2193@columbia.edu>
2018-03-14 22:16:22 -0400

    Style collection grid removal links

--- frontend/components/user_show/album_index_item.jsx
+++ frontend/components/user_show/album_index_item.jsx
@@ -11,16 +11,16 @@ const AlbumIndexItem = ({album, destroyCollecting}) => {
           <Link to={ `/albums/${album.id}` }>
             <img className='album-cover' src={ album.coverImgUrl } />
           </Link>
-          <h5 className='album-title'>{ album.title }</h5>
-          <span>by <Link to={ `/users/${album.artistId}` }>{ album.artistName }</Link></span>
-          {
-            collection &&
-              <button onClick={ () => destroyCollecting(album.id)}>
-                remove</button>
-          }
         </div>
         <div className='album-info-detail'>
+          <h5 className='album-title'>{ album.title }</h5>
+          <span>by <Link to={ `/users/${album.artistId}` }>{ album.artistName }</Link></span>
 
+          { collection &&
+            <button className='collection-index-btn'
+              onClick={ () => destroyCollecting(album.id)}>
+              remove</button>
+          }
         </div>
       </div>
     </li>
