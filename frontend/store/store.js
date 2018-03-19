98676bcc649bfac37d9501bd21df68e24addc1eb
cameron mcinnes <crm2193@columbia.edu>
2018-03-19 10:33:01 -0400

    Ready app for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,25 +3,19 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
+let middlesWares = applyMiddleware(thunk);
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  middlesWares = composeEnhancers(applyMiddleware(thunk, logger))
+}
+
 const configureStore = (preloadedState = {}) => {
-  // if (process.env.NODE_ENV === 'production') {
-  //   return createStore(
-  //     rootReducer,
-  //     preloadedState,
-  //     applyMiddleware(thunk, logger)
-  //   )
-  // } else {
-  //   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  //   return createStore(
-  //     rootReducer,
-  //     preloadedState,
-  //     composeEnhancers(applyMiddleware(thunk, logger))
-  //   )
-  // }
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    middlesWares
   )
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const webpack = require("webpack");
+
+let plugins = [];
+let devPlugins = [];
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -10,6 +31,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
